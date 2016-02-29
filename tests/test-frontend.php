9d5ee0e6c166d6204147e1c8838f1818c88d9db4
Gary Pendergast <gary@pento.net>
2016-02-29 23:44:54 +1100

    Add some initial unit tests

---
+++ tests/test-frontend.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Test frontend stuff.
+ *
+ * @package react
+ */
+
+class React_Test_Frontend extends WP_UnitTestCase {
+	/**
+	 * Test that the container is added to a post
+	 */
+	function test_container_exists() {
+		$post_id = $this->factory->post->create();
+
+		$this->go_to( get_permalink( $post_id ) );
+
+		ob_start();
+		the_content();
+		$content = ob_get_clean();
+
+		$this->assertGreaterThanOrEqual( 0, strpos( '<div class="emoji-reactions"', $content ) );
+	}
+
+	/**
+	 * Test that the Add Reaction button is added to a post
+	 */
+	function test_add_button_exists() {
+		$post_id = $this->factory->post->create();
+
+		$this->go_to( get_permalink( $post_id ) );
+
+		ob_start();
+		the_content();
+		$content = ob_get_clean();
+
+		$this->assertEquals( 1, preg_match( '/<div [^>]*class="emoji-reaction-add"/', $content ) );
+	}
+}
