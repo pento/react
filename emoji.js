e93711b03b8042584af3c3214523c107afddec9d
Gary Pendergast <gary@pento.net>
2016-02-29 20:36:27 +1100

    Let's not require jQuery
    Some basic structure for the emoji click handling

--- emoji.js
+++ emoji.js
@@ -1,7 +1,48 @@
-( function( $ ) {
+( function() {
 
-	$( document ).on( 'click', '.emoji-reaction', null, function( e ) {
-		
-	} );
+ 	var reactionClick = function( event ) {
+		var el;
 
-} )( jQuery );
+		event = event || window.event;
+
+		el = event.target || event.srcElement;
+
+		// Bail early, if we can.
+		if ( 'DIV' !== el.nodeName ) {
+			return true;
+		}
+
+		if ( ! node.className || typeof node.className !== 'string' ) {
+			return true;
+		}
+
+		if ( node.className.indexOf( 'emoji-reaction-add' ) !== -1 ) {
+			event.preventDefault();
+			showReactionPopup( el );
+			return false;
+		}
+
+		if ( node.className.indexOf( 'emoji-reaction' ) !== -1 ) {
+			event.preventDefault();
+			react( el );
+			return false;
+		}
+
+		return true;
+	}
+
+	var showReactionPopup = function( el ) {
+
+	};
+
+	var react = function( el ) {
+
+	};
+
+	if ( document.addEventListener ) {
+		document.addEventListener( "click", reactionClick );
+	} else {
+		document.attachEvent( "click", reactionClick );
+	}
+
+} )();
