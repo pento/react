0f61861712c752d25381dad9fab68def4fa513c3
Gary Pendergast <gary@pento.net>
2016-02-29 16:55:52 +1100

    Remove the emoji picker lib, because that was a poor choice.

--- react.php
+++ react.php
@@ -23,15 +23,8 @@ class React {
 	}
 
 	function enqueue() {
-		wp_enqueue_style( 'react-emoji-picker-nanoscroller', plugins_url( 'emoji-picker/lib/css/nanoscroller.css', __FILE__ ) );
-		wp_enqueue_style( 'react-emoji-picker-emoji', plugins_url( 'emoji-picker/lib/css/emoji.css', __FILE__ ) );
 		wp_enqueue_style( 'react-emoji', plugins_url( 'emoji.css', __FILE__ ) );
 
-		wp_enqueue_style( 'react-emoji-picker-nanoscroller', plugins_url( 'emoji-picker/lib/js/nanoscroller.min.js', __FILE__ ), array( 'jquery' ), false, true );
-		wp_enqueue_style( 'react-emoji-picker-tether', plugins_url( 'emoji-picker/lib/js/tether.min.js', __FILE__ ), array( 'jquery' ), false, true );
-		wp_enqueue_style( 'react-emoji-picker-config', plugins_url( 'emoji-picker/lib/js/config.js', __FILE__ ), array( 'jquery' ), false, true );
-		wp_enqueue_style( 'react-emoji-picker-util', plugins_url( 'emoji-picker/lib/js/util.js', __FILE__ ), array( 'jquery' ), false, true );
-		wp_enqueue_style( 'react-emoji-picker-jquery-emojiarea', plugins_url( 'emoji-picker/lib/js/query.emojiarea.js', __FILE__ ), array( 'jquery' ), false, true );
 		wp_enqueue_style( 'react-emoji-picker-emoji-picker', plugins_url( 'emoji-picker/lib/js/emoji-picker.js', __FILE__ ), array( 'jquery' ), false, true );
 	}
 
