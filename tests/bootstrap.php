ee4a1a193751d97c425f4994e4e008c2bcb224ff
Gary Pendergast <gary@pento.net>
2016-03-06 22:27:13 +1100

    Add a composer file, unit tests now load WP-API

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,13 +5,14 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
-require_once $_tests_dir . '/includes/functions.php';
+require_once( $_tests_dir . '/includes/functions.php' );
 
 /**
  * Load the plugin.
  */
 function _manually_load_react_plugin() {
-	require dirname( __FILE__ ) . '/../react.php';
+	require_once( dirname( __FILE__ ) . '/../vendor/json-rest-api/plugin.php' );
+	require_once( dirname( __FILE__ ) . '/../react.php' );
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_react_plugin' );
@@ -20,13 +21,13 @@ require $_tests_dir . '/includes/bootstrap.php';
 
 // Helper classes
 if ( ! class_exists( 'WP_Test_REST_TestCase' ) ) {
-	require_once dirname( __FILE__ ) . '/class-wp-test-rest-testcase.php';
+	require_once( dirname( __FILE__ ) . '/class-wp-test-rest-testcase.php' );
 }
 
 if ( ! class_exists( 'WP_Test_REST_Controller_Testcase' ) ) {
-	require_once dirname( __FILE__ ) . '/class-wp-test-rest-controller-testcase.php';
+	require_once( dirname( __FILE__ ) . '/class-wp-test-rest-controller-testcase.php' );
 }
 
 if ( ! class_exists( 'WP_Test_Spy_REST_Server' ) ) {
-	require_once dirname( __FILE__ ) . '/class-wp-test-spy-rest-server.php';
+	require_once( dirname( __FILE__ ) . '/class-wp-test-spy-rest-server.php' );
 }
